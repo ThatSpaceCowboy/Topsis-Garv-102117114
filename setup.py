883114c7b51800bcdd9e539663de103257a29aed
Spikey <41112158+ThatSpaceCowboy@users.noreply.github.com>
2024-01-29 00:26:33 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ setup(
   description = 'Contains the Topsis package by garv on topsis',   # Give a short description about your library
   author = 'garv grover',                   # Type in your name
   author_email = 'garvgrover65@gmail.com',      # Type in your E-Mail
-  url = 'https://github.com/Dhruv-os-23/Topsis-Garv-102117114',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/Dhruv-os-23/Topsis-Garv-102117114/archive/refs/tags/1.0.4.tar.gz',    # I explain this later on
+  url = 'https://github.com/ThatSpaceCowboy/Topsis-Garv-102117114',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/ThatSpaceCowboy/Topsis-Garv-102117114/archive/refs/tags/1.0.4.tar.gz',    # I explain this later on
   keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'pandas',
